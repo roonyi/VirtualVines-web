499bd9f869b7388e11e002fc6f752490c275be09
Roberto <roonyi@gmail.com>
2025-04-07 20:05:18 -0500

    .

--- src/components/myweek.jsx
+++ src/components/myweek.jsx
@@ -5,25 +5,28 @@ import Carousel from 'react-bootstrap/Carousel';
 
 export const MyWeek = (props) => {
      // Constant for the Carousels
-     const responsive = {
-        superLargeDesktop: {
-            // the naming can be any, depends on you.
-            breakpoint: { max: 4000, min: 3000 },
-            items: 5
-          },
-          desktop: {
-            breakpoint: { max: 3000, min: 1024 },
-            items: 3
-          },
-          tablet: {
-            breakpoint: { max: 1024, min: 464 },
-            items: 2
-          },
-          mobile: {
-            breakpoint: { max: 464, min: 0 },
-            items: 1
-          }
-      };
+     const prevIcon = (
+        <div style={{
+          borderRadius: '50%', 
+          padding: '10px',
+          color: 'white'
+        }}>
+          <svg width="24" height="24" fill="currentColor" viewBox="0 0 16 16">
+            <path fillRule="evenodd" d="M11.354 1.646a.5.5 0 0 1 0 .708L5.707 8l5.647 5.646a.5.5 0 0 1-.708.708l-6-6a.5.5 0 0 1 0-.708l6-6a.5.5 0 0 1 .708 0z"/>
+          </svg>
+        </div>
+      );
+    const nextIcon = (
+        <div style={{
+          borderRadius: '50%', 
+          padding: '10px',
+          color: 'white'
+        }}>
+          <svg width="24" height="24" fill="currentColor" viewBox="0 0 16 16">
+            <path fillRule="evenodd" d="M4.646 1.646a.5.5 0 0 1 .708 0l6 6a.5.5 0 0 1 0 .708l-6 6a.5.5 0 0 1-.708-.708L10.293 8 4.646 2.354a.5.5 0 0 1 0-.708z"/>
+          </svg>
+        </div>
+      );
 
     return(
         <>
@@ -34,24 +37,29 @@ export const MyWeek = (props) => {
                 </Row>
                 <Row className='myweek-row2'>
                     <Col className="d-flex justify-content-center">
-                        <div className="carousel-wrapper" style={{ maxWidth: '600px', width: '100%' }}>
+                        <div className="carousel-wrapper">
                             <Carousel 
                                 slide={true}
-                                interval={3000}
-                                controls={true}
+                                interval={4000}
                                 indicators={true}
+                                controls={true}
+                                prevIcon={prevIcon}
+                                nextIcon={nextIcon}
+                                prevLabel={""}
+                                nextLabel={""}
+                                className="corner-controls"
                             >
                                 <Carousel.Item>
                                     <img 
                                         src={props.myweek.card1} 
-                                        className="d-block w-100" 
+                                        className="card-img" 
                                         alt="First slide"
                                     />
                                 </Carousel.Item>
                                 <Carousel.Item>
                                     <img 
                                         src={props.myweek.card2} 
-                                        className="d-block w-100" 
+                                        className="card-img" 
                                         alt="Second slide"
                                     />
                                 </Carousel.Item>
